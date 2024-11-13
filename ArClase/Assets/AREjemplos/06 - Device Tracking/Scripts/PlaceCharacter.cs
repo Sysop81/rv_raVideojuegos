c925c5493efaca40639d9f273a3c93152aa327f1
José Ramón López <jose.rlg27@gmail.com>
2024-11-13 21:22:49 +0100

    Working on particle system. MurcielaGO

--- ArClase/Assets/AREjemplos/06 - Device Tracking/Scripts/PlaceCharacter.cs
+++ ArClase/Assets/AREjemplos/06 - Device Tracking/Scripts/PlaceCharacter.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlaceCharacter : MonoBehaviour
 {
     [SerializeField] private float distance;
-
+    [SerializeField] private ParticleSystem fireworksParticle;
     private Transform player;
+    private bool _isInit;
     
     // Start is called before the first frame update
     void Start()
@@ -23,8 +24,21 @@ public class PlaceCharacter : MonoBehaviour
 
     public void ChangePosition()
     {
+        StartCoroutine(ManageParticleSystem());
+
+        /*transform.position = new Vector3(Random.insideUnitSphere.x * distance,
+            transform.position.y,Random.insideUnitSphere.z * distance);
+        transform.LookAt(player);*/
+        
+    }
+
+    private IEnumerator ManageParticleSystem()
+    {
+        fireworksParticle.Play();
+        yield return new WaitForSeconds(fireworksParticle.main.duration);
         transform.position = new Vector3(Random.insideUnitSphere.x * distance,
             transform.position.y,Random.insideUnitSphere.z * distance);
         transform.LookAt(player);
     }
+    
 }
--- ArClase/Assets/AREjemplos/06 - Device Tracking/Scripts/TouchController.cs
+++ ArClase/Assets/AREjemplos/06 - Device Tracking/Scripts/TouchController.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 public class TouchController : MonoBehaviour
 {
     public UnityEvent OnTouch;
-
+    
     private void OnMouseDown()
     {
         OnTouch.Invoke();
